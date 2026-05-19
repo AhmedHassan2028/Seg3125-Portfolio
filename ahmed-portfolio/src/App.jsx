4641b44d276bd5243b3e3d503ad8a6f6d908b387
AhmedHassan2028 <ahfa871@gmail.com>
2026-05-19 00:36:10 -0400

    Navbar added

--- ahmed-portfolio/src/App.jsx
+++ ahmed-portfolio/src/App.jsx
@@ -4,12 +4,28 @@ function App() {
   
 
   return (
-    <nav className="navbar"> 
-      <div>My portfolio</div>
-      <div>My portfolio</div>
-      <div>My portfolio</div>
-      <button type="button" class="btn btn-primary">Primary</button>
-    </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Portfolio</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+        {/* <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> */}
+      </div>
+    </div>
+  </nav>
   )
 }
 
